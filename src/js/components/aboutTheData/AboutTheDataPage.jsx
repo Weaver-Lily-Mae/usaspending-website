720ee403959f21cfb3228e8f802038b2f8111728
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-11-23 18:41:52 -0500

    [DEV-6377] Period picker implementation:
    1. WithLatestFy now provides the actual array from the api
    of latest submission periods
    2. capturing & validating period selection in url
    3. Providing enabled/disabled styles
    4. Using data mapping to get the periods in shape
    
    NOTE on (3) above:
    FY's b4 2020 only allow for quarter selection.
    Special styles apply for this case per the mock.
    To conform to these styles, we have to style
    sibling elements. Sibling elements can only
    be styled when they come after the element you
    are selecting, so we put all the last periods of
    the quarter at the top of the list, then use
    the css property 'order' to make the order correct.
    
    This is kind of hacky and warrants this elaborate
    note.

--- src/js/components/aboutTheData/AboutTheDataPage.jsx
+++ src/js/components/aboutTheData/AboutTheDataPage.jsx
@@ -3,21 +3,19 @@
  * Created by Lizzie Salita 11/25/20
  */
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import { useParams } from "react-router-dom";
-import { TooltipComponent, TooltipWrapper, Tabs, Picker } from "data-transparency-ui";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { TooltipComponent, TooltipWrapper, Tabs } from "data-transparency-ui";
 
-import { allFiscalYears } from "helpers/fiscalYearHelper";
 import Header from "containers/shared/HeaderContainer";
 import Footer from "containers/Footer";
-import WithLatestFy from "containers/account/WithLatestFy";
 import StickyHeader from "components/sharedComponents/stickyHeader/StickyHeader";
 import Note from "components/sharedComponents/Note";
 import AboutTheDataModal from "components/aboutTheData/AboutTheDataModal";
 import AgenciesContainer from 'containers/aboutTheData/AgenciesContainer';
 import { modalTitles, modalClassNames } from 'dataMapping/aboutTheData/modals';
+import TimeFilters, { getPeriodWithTitleById } from "./TimeFilters";
 
 require("pages/aboutTheData/agenciesPage.scss");
 
@@ -46,11 +44,12 @@ TableTabLabel.propTypes = {
 const message = "All numeric figures in this table are calculated based on the set of TAS owned by each agency, as opposed to the set of TAS that the agency directly reported to USAspending.gov. In the vast majority of cases, these are exactly the same (upwards of 95% of TAS—with these TAS representing over 99% of spending—are submitted and owned by the same agency). This display decision is consistent with our practice throughout the website of grouping TAS by the owning agency rather than the reporting agency. While reporting agencies are not identified in this table, they are available in the Custom Account Download in the reporting_agency_name field.";
 
 const AboutTheDataPage = ({
-    dataAsOf,
     history
 }) => {
-    const { fy: urlFy } = useParams();
+    const { fy: urlFy, period: urlPeriod } = useParams();
     const [selectedFy, setSelectedFy] = useState(null);
+    const [selectedPeriod, setSelectedPeriod] = useState(null);
+
     const [activeTab, setActiveTab] = useState('details'); // details or dates
     const [showModal, setShowModal] = useState('');
     const [modalAgency, setModalAgency] = useState('');
@@ -60,34 +59,35 @@ const AboutTheDataPage = ({
         setShowModal(modalType);
         setModalAgency(agencyName);
     };
+
     const closeModal = () => setShowModal('');
+
+    const updateUrl = (newFy, newPeriod) => {
+        history.push({ pathname: `/about-the-data/agencies/${newFy}/${newPeriod}` });
+    };
 
     const handleSwitchTab = (tab) => {
         setActiveTab(tab);
     };
 
-    const handleFyChange = (fy) => {
-        history.push(`${fy}`);
-        setSelectedFy(fy);
+    const onTimeFilterSelection = (fy, p = urlPeriod) => {
+        const newPeriodWithTitle = typeof p === 'object'
+            ? p
+            : getPeriodWithTitleById(p);
+        if (selectedFy !== fy && selectedPeriod?.id !== newPeriodWithTitle.id) {
+            setSelectedPeriod(p);
+            setSelectedFy(fy);
+            updateUrl(fy, newPeriodWithTitle.id);
+        }
+        else if (selectedPeriod?.id === newPeriodWithTitle.id) {
+            setSelectedFy(fy);
+            updateUrl(fy, newPeriodWithTitle.id);
+        }
+        else if (fy === selectedFy) {
+            setSelectedPeriod(newPeriodWithTitle);
+            updateUrl(fy, newPeriodWithTitle.id);
+        }
     };
-
-    useEffect(() => {
-        if (dataAsOf) {
-            setSelectedFy(dataAsOf.year());
-        }
-    }, [dataAsOf]);
-
-    useEffect(() => {
-        if (dataAsOf) {
-            if (allFiscalYears(2017, dataAsOf.year()).includes(parseInt(urlFy, 10))) {
-                setSelectedFy(urlFy);
-            }
-            else {
-                setSelectedFy(`${dataAsOf.year()}`);
-                history.push('latest');
-            }
-        }
-    }, [dataAsOf]);
 
     return (
         <div className="usa-da__about-the-data__agencies-page">
@@ -110,23 +110,13 @@ const AboutTheDataPage = ({
                             { internal: 'details', label: <TableTabLabel label="Statistics by Reporting Period" /> },
                             { internal: 'dates', label: <TableTabLabel label="Updates by  Fiscal Year" /> }
                         ]} />
-                    <div className="table-controls__time-and-search">
-                        <span className="fy-picker-title">FISCAL YEAR</span>
-                        <Picker
-                            icon=""
-                            isFixedWidth
-                            selectedOption={selectedFy
-                                ? `FY ${selectedFy}`
-                                : (
-                                    <div className="fy-loading">
-                                        FY <FontAwesomeIcon icon="spinner" size="sm" alt="Toggle menu" spin />
-                                    </div>
-                                )}
-                            options={dataAsOf
-                                ? allFiscalYears(2017, dataAsOf.year()).map((year) => ({ name: `${year}`, value: `${year}`, onClick: handleFyChange }))
-                                : [{ name: 'Loading...', value: null, onClick: () => {} }]
-                            } />
-                    </div>
+                    <TimeFilters
+                        activeTab={activeTab}
+                        onTimeFilterSelection={onTimeFilterSelection}
+                        selectedPeriod={selectedPeriod}
+                        selectedFy={selectedFy}
+                        urlPeriod={urlPeriod}
+                        urlFy={urlFy} />
                 </div>
                 <AgenciesContainer openModal={modalClick} activeTab={activeTab} />
                 <Note message={message} />
@@ -147,12 +137,7 @@ const AboutTheDataPage = ({
 };
 
 AboutTheDataPage.propTypes = {
-    dataAsOf: PropTypes.oneOfType([PropTypes.object, null]),
     history: PropTypes.object
 };
 
-export default (props) => (
-    <WithLatestFy propName="dataAsOf">
-        <AboutTheDataPage {...props} />
-    </WithLatestFy>
-);
+export default AboutTheDataPage;
